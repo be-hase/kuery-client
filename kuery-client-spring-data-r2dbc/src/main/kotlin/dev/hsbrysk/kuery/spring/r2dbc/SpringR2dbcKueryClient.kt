22e29323beaddbd8b8dc373faab840d7884cb906
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-06-06 22:48:33 +0900

    polish

--- kuery-client-spring-data-r2dbc/src/main/kotlin/dev/hsbrysk/kuery/spring/r2dbc/SpringR2dbcKueryClient.kt
+++ kuery-client-spring-data-r2dbc/src/main/kotlin/dev/hsbrysk/kuery/spring/r2dbc/SpringR2dbcKueryClient.kt
@@ -1,10 +1,14 @@
 package dev.hsbrysk.kuery.spring.r2dbc
 
-import dev.hsbrysk.kuery.core.KueryClient
 import dev.hsbrysk.kuery.spring.r2dbc.internal.DefaultSpringR2dbcKueryClientBuilder
+import reactor.util.context.ContextView
 
 object SpringR2dbcKueryClient {
-    val SQL_ID_REACTOR_CONTEXT_KEY = "${KueryClient::class.java.name}:sqlId"
+    val SQL_ID_REACTOR_CONTEXT_KEY = "${SpringR2dbcKueryClient::class.java.name}:sqlId"
+
+    fun sqlId(context: ContextView): String? {
+        return context.getOrEmpty<String>(SQL_ID_REACTOR_CONTEXT_KEY).orElse(null)
+    }
 
     fun builder(): SpringR2dbcKueryClientBuilder {
         return DefaultSpringR2dbcKueryClientBuilder()
