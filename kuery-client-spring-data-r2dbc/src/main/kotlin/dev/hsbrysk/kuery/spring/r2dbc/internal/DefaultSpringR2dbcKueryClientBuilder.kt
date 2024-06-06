361080ef914c70c9c0961a40a66f71775d26c1f8
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-06-06 22:45:31 +0900

    polish

--- kuery-client-spring-data-r2dbc/src/main/kotlin/dev/hsbrysk/kuery/spring/r2dbc/internal/DefaultSpringR2dbcKueryClientBuilder.kt
+++ kuery-client-spring-data-r2dbc/src/main/kotlin/dev/hsbrysk/kuery/spring/r2dbc/internal/DefaultSpringR2dbcKueryClientBuilder.kt
@@ -3,19 +3,15 @@ package dev.hsbrysk.kuery.spring.r2dbc.internal
 import dev.hsbrysk.kuery.core.KueryClient
 import dev.hsbrysk.kuery.spring.r2dbc.SpringR2dbcKueryClientBuilder
 import io.r2dbc.spi.ConnectionFactory
-import org.springframework.core.convert.ConversionService
 import org.springframework.core.convert.support.DefaultConversionService
-import org.springframework.data.convert.CustomConversions.StoreConversions
 import org.springframework.data.r2dbc.convert.R2dbcCustomConversions
 import org.springframework.data.r2dbc.dialect.DialectResolver
-import org.springframework.data.r2dbc.dialect.R2dbcDialect
 import org.springframework.r2dbc.core.DatabaseClient
 
 internal class DefaultSpringR2dbcKueryClientBuilder : SpringR2dbcKueryClientBuilder {
     private var connectionFactory: ConnectionFactory? = null
     private var converters: List<Any> = emptyList()
     private var databaseClient: DatabaseClient? = null
-    private var conversionService: ConversionService? = null
 
     override fun connectionFactory(connectionFactory: ConnectionFactory): SpringR2dbcKueryClientBuilder {
         this.connectionFactory = connectionFactory
@@ -32,15 +28,12 @@ internal class DefaultSpringR2dbcKueryClientBuilder : SpringR2dbcKueryClientBuil
         return this
     }
 
-    override fun conversionService(conversionService: ConversionService): SpringR2dbcKueryClientBuilder {
-        this.conversionService = conversionService
-        return this
-    }
-
     override fun build(): KueryClient {
         val databaseClient = this.databaseClient ?: defaultDatabaseClient()
-        val conversionService = this.conversionService ?: defaultConversionService()
-        return DefaultSpringR2dbcKueryClient(databaseClient, conversionService)
+        val conversionService = DefaultConversionService()
+        val customConversions = r2dbcCustomConversions()
+        customConversions.registerConvertersIn(conversionService)
+        return DefaultSpringR2dbcKueryClient(databaseClient, conversionService, customConversions)
     }
 
     private fun defaultDatabaseClient(): DatabaseClient {
@@ -53,28 +46,10 @@ internal class DefaultSpringR2dbcKueryClientBuilder : SpringR2dbcKueryClientBuil
             .build()
     }
 
-    private fun defaultConversionService(): ConversionService {
-        val conversionService = DefaultConversionService()
-        val customConversions = r2dbcCustomConversions()
-        customConversions.registerConvertersIn(conversionService)
-        return conversionService
-    }
-
-    // ref: https://github.com/spring-projects/spring-data-relational/blob/abd0c85629756d34b98ca13b2a3eff341b832d25/spring-data-r2dbc/src/main/java/org/springframework/data/r2dbc/config/AbstractR2dbcConfiguration.java#L236-L238
     private fun r2dbcCustomConversions(): R2dbcCustomConversions {
-        return R2dbcCustomConversions(storeConversions(), converters)
-    }
-
-    // ref: https://github.com/spring-projects/spring-data-relational/blob/abd0c85629756d34b98ca13b2a3eff341b832d25/spring-data-r2dbc/src/main/java/org/springframework/data/r2dbc/config/AbstractR2dbcConfiguration.java#L254-L262
-    private fun storeConversions(): StoreConversions {
         val connectionFactory = requireNotNull(databaseClient?.connectionFactory ?: connectionFactory) {
             "Specify either databaseClient or connectionFactory."
         }
-        val dialect: R2dbcDialect = DialectResolver.getDialect(connectionFactory)
-        val converters = buildList {
-            addAll(dialect.converters)
-            addAll(R2dbcCustomConversions.STORE_CONVERTERS)
-        }
-        return StoreConversions.of(dialect.simpleTypeHolder, converters)
+        return R2dbcCustomConversions.of(DialectResolver.getDialect(connectionFactory), converters)
     }
 }
