9c984da7a2de7dee7bbbc7219b89a3a4fd9c4b4a
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-06-08 15:29:37 +0900

    polish

--- kuery-client-spring-data-jdbc/build.gradle.kts
+++ kuery-client-spring-data-jdbc/build.gradle.kts
@@ -7,4 +7,7 @@ plugins {
 dependencies {
     implementation(platform(libs.spring.boot.bom))
     implementation("org.springframework.data:spring-data-jdbc")
+
+    testImplementation("org.testcontainers:mysql")
+    testImplementation("com.mysql:mysql-connector-j")
 }
