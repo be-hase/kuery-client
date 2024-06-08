23878cb6abeb8106481d2058fa17698ed82c5727
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-06-08 12:46:25 +0900

    polish

--- kuery-client-core/src/main/kotlin/dev/hsbrysk/kuery/core/KueryBlockingClient.kt
+++ kuery-client-core/src/main/kotlin/dev/hsbrysk/kuery/core/KueryBlockingClient.kt
@@ -7,32 +7,66 @@ interface KueryBlockingClient {
     fun sql(block: SqlDsl.() -> Unit): FetchSpec
 
     interface FetchSpec {
+        /**
+         * Receives the results as a map.
+         */
         fun singleMap(): Map<String, Any?>
 
-        fun <T : Any> single(returnType: KClass<T>): T
-
+        /**
+         * Receives the results as a map.
+         */
         fun singleMapOrNull(): Map<String, Any?>?
 
+        /**
+         * Receives the results converted to the specified type.
+         */
+        fun <T : Any> single(returnType: KClass<T>): T
+
+        /**
+         * Receives the results converted to the specified type.
+         */
         fun <T : Any> singleOrNull(returnType: KClass<T>): T?
 
+        /**
+         * Receives the results of multiple rows as a map.
+         */
         fun listMap(): List<Map<String, Any?>>
 
+        /**
+         * Receives the results of multiple rows converted to the specified type.
+         */
         fun <T : Any> list(returnType: KClass<T>): List<T>
 
+        /**
+         * Contract for fetching the number of affected rows
+         */
         fun rowsUpdated(): Long
 
+        /**
+         * Receives the values generated on the database side.
+         * For example, an auto increment value.
+         */
         fun generatedValues(vararg columns: String): Map<String, Any>
     }
 }
 
+/**
+ * Receives the results converted to the specified type.
+ */
 inline fun <reified T : Any> FetchSpec.single(): T {
     return single(T::class)
 }
 
+/**
+ * Receives the results converted to the specified type.
+ */
 inline fun <reified T : Any> FetchSpec.singleOrNull(): T? {
     return singleOrNull(T::class)
 }
 
+/**
+ * Receives the results of multiple rows converted to the specified type.
+ */
 inline fun <reified T : Any> FetchSpec.list(): List<T> {
     return list(T::class)
 }
