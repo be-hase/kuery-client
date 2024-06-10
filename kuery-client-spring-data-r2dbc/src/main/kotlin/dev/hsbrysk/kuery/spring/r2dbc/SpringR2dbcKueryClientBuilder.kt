c18bec2950e67348e879c285f3844fdc0e4cc03d
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-06-10 15:19:09 +0900

    observation with kuery-client-spring-data-r2dbc

--- kuery-client-spring-data-r2dbc/src/main/kotlin/dev/hsbrysk/kuery/spring/r2dbc/SpringR2dbcKueryClientBuilder.kt
+++ kuery-client-spring-data-r2dbc/src/main/kotlin/dev/hsbrysk/kuery/spring/r2dbc/SpringR2dbcKueryClientBuilder.kt
@@ -1,12 +1,20 @@
 package dev.hsbrysk.kuery.spring.r2dbc
 
 import dev.hsbrysk.kuery.core.KueryClient
+import dev.hsbrysk.kuery.core.observation.KueryClientFetchObservationConvention
+import io.micrometer.observation.ObservationRegistry
 import io.r2dbc.spi.ConnectionFactory
 
 interface SpringR2dbcKueryClientBuilder {
     fun connectionFactory(connectionFactory: ConnectionFactory): SpringR2dbcKueryClientBuilder
 
     fun converters(converters: List<Any>): SpringR2dbcKueryClientBuilder
+
+    fun observationRegistry(observationRegistry: ObservationRegistry): SpringR2dbcKueryClientBuilder
+
+    fun observationConvention(
+        observationConvention: KueryClientFetchObservationConvention,
+    ): SpringR2dbcKueryClientBuilder
 
     fun build(): KueryClient
 }
--- kuery-client-spring-data-r2dbc/src/main/kotlin/dev/hsbrysk/kuery/spring/r2dbc/internal/DefaultSpringR2dbcKueryClientBuilder.kt
+++ kuery-client-spring-data-r2dbc/src/main/kotlin/dev/hsbrysk/kuery/spring/r2dbc/internal/DefaultSpringR2dbcKueryClientBuilder.kt
@@ -1,7 +1,9 @@
 package dev.hsbrysk.kuery.spring.r2dbc.internal
 
 import dev.hsbrysk.kuery.core.KueryClient
+import dev.hsbrysk.kuery.core.observation.KueryClientFetchObservationConvention
 import dev.hsbrysk.kuery.spring.r2dbc.SpringR2dbcKueryClientBuilder
+import io.micrometer.observation.ObservationRegistry
 import io.r2dbc.spi.ConnectionFactory
 import org.springframework.core.convert.support.DefaultConversionService
 import org.springframework.data.r2dbc.convert.R2dbcCustomConversions
@@ -11,6 +13,8 @@ import org.springframework.r2dbc.core.DatabaseClient
 internal class DefaultSpringR2dbcKueryClientBuilder : SpringR2dbcKueryClientBuilder {
     private var connectionFactory: ConnectionFactory? = null
     private var converters: List<Any> = emptyList()
+    private var observationRegistry: ObservationRegistry? = null
+    private var observationConvention: KueryClientFetchObservationConvention? = null
 
     override fun connectionFactory(connectionFactory: ConnectionFactory): SpringR2dbcKueryClientBuilder {
         this.connectionFactory = connectionFactory
@@ -19,6 +23,18 @@ internal class DefaultSpringR2dbcKueryClientBuilder : SpringR2dbcKueryClientBuil
 
     override fun converters(converters: List<Any>): SpringR2dbcKueryClientBuilder {
         this.converters = converters
+        return this
+    }
+
+    override fun observationRegistry(observationRegistry: ObservationRegistry): SpringR2dbcKueryClientBuilder {
+        this.observationRegistry = observationRegistry
+        return this
+    }
+
+    override fun observationConvention(
+        observationConvention: KueryClientFetchObservationConvention,
+    ): SpringR2dbcKueryClientBuilder {
+        this.observationConvention = observationConvention
         return this
     }
 
@@ -33,7 +49,13 @@ internal class DefaultSpringR2dbcKueryClientBuilder : SpringR2dbcKueryClientBuil
             registerConvertersIn(conversionService)
         }
 
-        return DefaultSpringR2dbcKueryClient(databaseClient, conversionService, customConversions)
+        return DefaultSpringR2dbcKueryClient(
+            databaseClient,
+            conversionService,
+            customConversions,
+            observationRegistry,
+            observationConvention,
+        )
     }
 
     private fun databaseClient(connectionFactory: ConnectionFactory): DatabaseClient {
