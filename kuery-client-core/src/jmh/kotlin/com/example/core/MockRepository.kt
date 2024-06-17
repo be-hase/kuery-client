46b381c94e238c955ed73ea348358e72b8d49d4b
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-06-17 22:17:48 +0900

    Add JMH Benchmark for Checking SqlId Generation Overhead

---
+++ kuery-client-core/src/jmh/kotlin/com/example/core/MockRepository.kt
@@ -0,0 +1,13 @@
+package com.example.core
+
+import dev.hsbrysk.kuery.core.Sql
+
+class MockRepository(private val client: MockKueryClient) {
+    fun select(id: Int): Pair<String, Sql> {
+        return client.sql("sqlId") { +"SELECT * FROM users WHERE id = ${bind(id)}" }
+    }
+
+    fun selectWithAutoIdGeneration(id: Int): Pair<String, Sql> {
+        return client.sql { +"SELECT * FROM users WHERE id = ${bind(id)}" }
+    }
+}
