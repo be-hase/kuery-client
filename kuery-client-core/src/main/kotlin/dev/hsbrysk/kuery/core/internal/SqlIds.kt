3540c159ee8c8edf1f7229d89168ba3207c2ce1b
Ryosuke Hasebe <hsb.1014@gmail.com>
2024-06-18 22:58:57 +0900

    Improve SqlIds (#20)

--- kuery-client-core/src/main/kotlin/dev/hsbrysk/kuery/core/internal/SqlIds.kt
+++ kuery-client-core/src/main/kotlin/dev/hsbrysk/kuery/core/internal/SqlIds.kt
@@ -8,6 +8,11 @@ object SqlIds {
 
     private val CACHE: ConcurrentHashMap<Class<*>, String> = ConcurrentHashMap()
 
+    private val SUFFIXES = listOf(
+        ".invokeSuspend",
+        "${'$'}suspendImpl",
+    )
+
     /**
      * Uses StackWalker to retrieve the caller.
      */
@@ -29,12 +34,21 @@ object SqlIds {
                 return@computeIfAbsent "UNKNOWN"
             }
 
-            val parts = name.removeSuffix(".invokeSuspend").split("$", ".").filterNot { it.matches(NUMBER_REGEX) }
+            val parts = name.removeSuffixes(SUFFIXES).split("$", ".").filterNot { it.matches(NUMBER_REGEX) }
             if (parts.isEmpty()) {
                 "UNKNOWN"
             } else {
                 parts.joinToString(".")
             }
         }
+    }
+
+    internal fun String.removeSuffixes(suffixes: List<String>): String {
+        suffixes.forEach { suffix ->
+            if (this.endsWith(suffix)) {
+                return this.removeSuffix(suffix)
+            }
+        }
+        return this
     }
 }
