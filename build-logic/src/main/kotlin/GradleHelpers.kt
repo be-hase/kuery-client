266b7df54af5c20e521d7bcdc777cc990f4449ae
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-08-23 12:33:22 +0900

    incr version

--- build-logic/src/main/kotlin/GradleHelpers.kt
+++ build-logic/src/main/kotlin/GradleHelpers.kt
@@ -1,3 +1,3 @@
 val isOnCI = System.getenv()["GITHUB_ACTIONS"] != null
 
-val KUERY_CLIENT_VERSION = "0.4.2" + if (isOnCI) "" else "-SNAPSHOT"
+val KUERY_CLIENT_VERSION = "0.5.0" + if (isOnCI) "" else "-SNAPSHOT"
