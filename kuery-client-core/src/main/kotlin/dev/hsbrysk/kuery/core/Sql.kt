054cdd336b7546a4ad22b8bf0419811ae4722b5b
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-12-24 10:31:54 +0900

    Introduce factory function for Sql and NamedSqlParameter

--- kuery-client-core/src/main/kotlin/dev/hsbrysk/kuery/core/Sql.kt
+++ kuery-client-core/src/main/kotlin/dev/hsbrysk/kuery/core/Sql.kt
@@ -18,6 +18,10 @@ interface Sql {
         /**
          * Create [Sql]
          */
+        @Deprecated(
+            message = "Use `dev.hsbrysk.kuery.core.Sql` function instead.",
+            replaceWith = ReplaceWith("Sql(body, parameters)"),
+        )
         fun of(
             body: String,
             parameters: List<NamedSqlParameter>,
@@ -26,10 +30,31 @@ interface Sql {
         /**
          * Create [Sql] using [SqlBuilder]
          */
+        @Deprecated(
+            message = "Use `dev.hsbrysk.kuery.core.Sql` function instead.",
+            replaceWith = ReplaceWith("Sql(block)"),
+        )
         fun create(block: SqlBuilder.() -> Unit): Sql {
             val builder = DefaultSqlBuilder()
             block(builder)
             return builder.build()
         }
     }
+}
+
+/**
+ * Create [Sql]
+ */
+fun Sql(
+    body: String,
+    parameters: List<NamedSqlParameter> = emptyList(),
+): Sql = DefaultSql(body, parameters)
+
+/**
+ * Create [Sql] using [SqlBuilder]
+ */
+fun Sql(block: SqlBuilder.() -> Unit): Sql {
+    val builder = DefaultSqlBuilder()
+    block(builder)
+    return builder.build()
 }
