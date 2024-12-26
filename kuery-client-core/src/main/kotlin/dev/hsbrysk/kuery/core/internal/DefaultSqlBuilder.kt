31468e377713138fbbbeec65367fc5435707860e
Ryosuke Hasebe <hsb.1014@gmail.com>
2024-12-26 13:52:22 +0900

    Add guard logic to `DefaultSqlBuilder` (#125)

--- kuery-client-core/src/main/kotlin/dev/hsbrysk/kuery/core/internal/DefaultSqlBuilder.kt
+++ kuery-client-core/src/main/kotlin/dev/hsbrysk/kuery/core/internal/DefaultSqlBuilder.kt
@@ -37,11 +37,20 @@ internal class DefaultSqlBuilder : SqlBuilder {
     fun interpolate(
         fragments: List<String>,
         values: List<Any?>,
-    ): String = buildString {
-        fragments.forEachIndexed { index, fragment ->
-            append(fragment)
-            if (index < values.size) {
-                append(bind(values[index]))
+    ): String {
+        val fragmentsSize = fragments.size
+        val valuesSize = values.size
+        // `StringConcatenationProcessor` should adhere to this assumption, but I'll double-check just to be sure.
+        check(fragmentsSize == valuesSize || fragmentsSize == valuesSize + 1) {
+            "The number of elements in `fragments`($fragmentsSize) and `values`($valuesSize) is incorrect. " +
+                "There might be an issue with the Kotlin compiler plugin."
+        }
+        return buildString {
+            fragments.forEachIndexed { index, fragment ->
+                append(fragment)
+                if (index < valuesSize) {
+                    append(bind(values[index]))
+                }
             }
         }
     }
