fa98c78807cb2b9c34b7047da565d256dc61d4f6
Ryosuke Hasebe <hsb.1014@gmail.com>
2025-02-10 12:23:05 +0900

    Remove kuery-client-core dependency from kuery-client-compiler (#143)

--- kuery-client-compiler/build.gradle.kts
+++ kuery-client-compiler/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 description = "Compiler plugin for the Kuery client."
 
 dependencies {
-    implementation(projects.kueryClientCore)
     implementation(kotlin("compiler-embeddable"))
     testImplementation(libs.kotlin.compile.testing)
 }
--- kuery-client-compiler/src/main/kotlin/dev/hsbrysk/kuery/compiler/ir/misc/ClassNames.kt
+++ kuery-client-compiler/src/main/kotlin/dev/hsbrysk/kuery/compiler/ir/misc/ClassNames.kt
@@ -1,8 +1,6 @@
 package dev.hsbrysk.kuery.compiler.ir.misc
 
-import dev.hsbrysk.kuery.core.SqlBuilder
-
 internal object ClassNames {
     val STRING = checkNotNull(String::class.qualifiedName)
-    val SQL_BUILDER = checkNotNull(SqlBuilder::class.qualifiedName)
+    const val SQL_BUILDER = "dev.hsbrysk.kuery.core.SqlBuilder"
 }
